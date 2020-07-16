6cb8134e9c1f9c752176da4f92b4aa179da62e3d
Pig Fang <g-plane@hotmail.com>
2020-07-16 14:51:44 +0800

    hoist integration tests

--- tests/i18n.rs
+++ tests/i18n.rs
@@ -1,39 +1,36 @@
-#[cfg(test)]
-mod i18n {
-    #[tokio::test]
-    async fn top_level_string() {
-        let text = nano::i18n::trans("./tests/i18n", "general.title", "en")
-            .await
-            .unwrap();
-        assert_eq!(text, "Title".to_string());
+#[tokio::test]
+async fn top_level_string() {
+    let text = nano::i18n::trans("./tests/i18n", "general.title", "en")
+        .await
+        .unwrap();
+    assert_eq!(text, "Title".to_string());
 
-        let text = nano::i18n::trans("./tests/i18n", "Namespace::general.title", "en")
-            .await
-            .unwrap();
-        assert_eq!(text, "Title".to_string());
-    }
+    let text = nano::i18n::trans("./tests/i18n", "Namespace::general.title", "en")
+        .await
+        .unwrap();
+    assert_eq!(text, "Title".to_string());
+}
 
-    #[tokio::test]
-    async fn nested_string() {
-        let text = nano::i18n::trans("./tests/i18n", "general.nested.key", "en")
-            .await
-            .unwrap();
-        assert_eq!(text, "value".to_string());
+#[tokio::test]
+async fn nested_string() {
+    let text = nano::i18n::trans("./tests/i18n", "general.nested.key", "en")
+        .await
+        .unwrap();
+    assert_eq!(text, "value".to_string());
 
-        let text = nano::i18n::trans("./tests/i18n", "Namespace::general.nested.key", "en")
-            .await
-            .unwrap();
-        assert_eq!(text, "value".to_string());
-    }
+    let text = nano::i18n::trans("./tests/i18n", "Namespace::general.nested.key", "en")
+        .await
+        .unwrap();
+    assert_eq!(text, "value".to_string());
+}
 
-    #[tokio::test]
-    async fn failures() {
-        let result = nano::i18n::trans("./tests/i18n", "", "en").await;
-        assert!(result.is_err());
+#[tokio::test]
+async fn failures() {
+    let result = nano::i18n::trans("./tests/i18n", "", "en").await;
+    assert!(result.is_err());
 
-        let result = nano::i18n::trans("./tests/i18n", "general.yuuko", "en")
-            .await
-            .unwrap();
-        assert_eq!(result, "general.yuuko");
-    }
+    let result = nano::i18n::trans("./tests/i18n", "general.yuuko", "en")
+        .await
+        .unwrap();
+    assert_eq!(result, "general.yuuko");
 }
