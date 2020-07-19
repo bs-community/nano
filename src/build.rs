a8de2817c4b565d2961c322a47b29a3c11c22dec
Pig Fang <g-plane@hotmail.com>
2020-07-19 11:40:10 +0800

    clean up after build

--- src/build.rs
+++ src/build.rs
@@ -1,5 +1,9 @@
-use std::io::{Error, ErrorKind};
-use tokio::{io::Result, process::Command};
+use futures::future::{join_all, try_join3};
+use std::{
+    io::{Error, ErrorKind},
+    path::Path,
+};
+use tokio::{fs, io::Result, process::Command};
 
 async fn yarn() -> Result<()> {
     info!("Running Yarn to install dependencies...");
@@ -39,7 +43,51 @@ async fn webpack() -> Result<()> {
     Ok(())
 }
 
-pub async fn build() -> Result<()> {
+async fn remove_ts_files(path: impl AsRef<Path>) -> Result<()> {
+    let mut items = fs::read_dir(&path).await?;
+    while let Some(item) = items.next_entry().await? {
+        let path = item.path();
+        let file_type = item.file_type().await?;
+        if file_type.is_file() && path.extension().map(|ext| ext == "ts").unwrap_or(false) {
+            fs::remove_file(path).await?;
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn clean_up(path: impl AsRef<Path>) -> Result<()> {
+    let path = path.as_ref().display();
+
+    let node_modules = fs::remove_dir_all(format!("{}/node_modules", path));
+    let git_ignore = fs::remove_file(format!("{}/.gitignore", path));
+    let ts_files = remove_ts_files(format!("{}/assets", path));
+
+    try_join3(node_modules, git_ignore, ts_files)
+        .await
+        .map(|_| ())
+}
+
+pub async fn build<S: AsRef<str>>(
+    root: impl AsRef<Path>,
+    plugins: impl Iterator<Item = (S, S)>,
+) -> Result<()> {
     yarn().await?;
-    webpack().await
+    webpack().await?;
+
+    let root = root.as_ref();
+
+    let cleans = plugins.map(|(name, _)| {
+        let name = name.as_ref();
+        info!("Cleaning up for plugin '{}'...", name);
+        let path = format!("{}/plugins/{}", root.display(), name);
+        async move {
+            if let Err(_) = clean_up(&path).await {
+                warn!("Failed to clean up files at '{}'", path);
+            }
+        }
+    });
+    join_all(cleans.collect::<Vec<_>>()).await;
+
+    Ok(())
 }
---
+++ tests/build.rs
@@ -0,0 +1,47 @@
+use futures::future::try_join;
+use std::{env::temp_dir, io::ErrorKind};
+use tokio::{fs, io::Result};
+
+#[tokio::test]
+async fn clean_up() -> Result<()> {
+    let mut path = temp_dir();
+    path.push("clean_up-test");
+
+    match fs::remove_dir_all(&path).await {
+        Ok(_) => {}
+        Err(e) if e.kind() == ErrorKind::NotFound => {}
+        e => panic!(e),
+    };
+
+    let display = path.display();
+
+    let node_modules = format!("{}/node_modules", display);
+    fs::create_dir_all(&node_modules).await?;
+
+    let git_ignore = format!("{}/.gitignore", display);
+    fs::write(&git_ignore, b"").await?;
+
+    let assets = format!("{}/assets", display);
+    fs::create_dir(&assets).await?;
+    let js = format!("{}/file.js", assets);
+    let ts = format!("{}/file.ts", assets);
+    try_join(fs::write(&js, b""), fs::write(&ts, b"")).await?;
+
+    nano::build::clean_up(&path).await?;
+
+    assert_eq!(
+        fs::File::open(&node_modules).await.unwrap_err().kind(),
+        ErrorKind::NotFound
+    );
+    assert_eq!(
+        fs::File::open(&git_ignore).await.unwrap_err().kind(),
+        ErrorKind::NotFound
+    );
+    assert!(fs::File::open(&js).await.is_ok());
+    assert_eq!(
+        fs::File::open(&ts).await.unwrap_err().kind(),
+        ErrorKind::NotFound
+    );
+
+    Ok(())
+}
